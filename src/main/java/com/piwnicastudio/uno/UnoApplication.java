a2b5e2588e96429382aadafb267ae5af51cd4f90
Kacper <kacper.kuczminski@gmail.com>
2025-08-11 22:33:27 +0200

    Some import cleanup

--- src/main/java/com/piwnicastudio/uno/UnoApplication.java
+++ src/main/java/com/piwnicastudio/uno/UnoApplication.java
@@ -1,11 +1,8 @@
 package com.piwnicastudio.uno;
 
 import com.piwnicastudio.uno.player.Player;
-import jakarta.persistence.Id;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 
 import java.util.List;
 
